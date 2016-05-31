4358fb380f5b960815aacb1c9fe1e73fcc71446d
Brock Renninger <brock.renninger@mc02ll4l6fh00.readytalk.com>
2016-05-31 11:55:55 -0600

    removed code

--- index.js
+++ index.js
@@ -73,9 +73,9 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 
 controller.hears('groupLunch', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Would you like to set up a group lunch?')
-  crontroller.hears('Yes', '.*', function (bot, message) {
-    bot.reply(message, 'Excellent')
-  })
+//  crontroller.hears('Yes', '.*', function (bot, message) {
+//    bot.reply(message, 'Excellent')
+//  })
 })
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
