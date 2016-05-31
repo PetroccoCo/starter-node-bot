814c049f24dd0a961d1c8535363b1fafb8c27cab
Dain <dain.darnell@readytalk.com>
2016-05-31 17:36:28 -0600

    Add list function for foursquare to return a list of objects

--- foursquare/index.js
+++ foursquare/index.js
@@ -1,46 +1,63 @@
-exports.start = function () {
-  let config = {
-    'secrets' : {
-      'clientId' : 'HT3MFIAJOSWFXKFGXPHY5X4CKLKEMQB5QX033NNE0M4LWTO0',
-      'clientSecret' : 'BYBWH053TYIAOPIVZZU2F3H0UBV1SEFTFCHT4MGVXCK3C21Y',
-      'redirectUrl' : 'https://localhost:3000'
-    },
-    'foursquare' : {
-      'version' : '20140806'
-    }
+const config = {
+  'secrets' : {
+    'clientId' : 'HT3MFIAJOSWFXKFGXPHY5X4CKLKEMQB5QX033NNE0M4LWTO0',
+    'clientSecret' : 'BYBWH053TYIAOPIVZZU2F3H0UBV1SEFTFCHT4MGVXCK3C21Y',
+    'redirectUrl' : 'https://localhost:3000'
+  },
+  'foursquare' : {
+    'version' : '20140806'
+  }
+}
+
+const LOCATION = {
+  name: 'Denver,CO',
+  lattitude: '39.753298',
+  longitude: '-105.003394'
+}
+
+const RADIUS = 1000
+//const FOOD_CATEGORY = '4d4b7105d754a06374d81259'
+const RESTAURANT_CATEGORY = '4d4b7105d754a06374d81259'
+
+const foursquare = require('node-foursquare')(config)
+
+exports.query = function(query, callback, limit = 5) {
+  let params = {
+    query: query,
+    limit: limit,
+    radius: RADIUS
   }
 
-  const LOCATION = 'Denver,CO'
-  let foursquare = require('node-foursquare')(config)
+  searchRestaurants(params, callback)
+}
 
-  function query(query, callback, limit = 5) {
-    let params = {
-      query: query,
-      limit: limit
-    }
-
-    searchVenues(params, callback)
+exports.list = function(callback) {
+  let params = {
+    limit: 50,
+    radius: RADIUS
   }
 
-  function searchVenues(params, callback) {
-    foursquare.Venues.search(null, null, LOCATION, params, null, function(error, data) {
-      if (error) {
-        console.log('Unable to retrieve venues', error)
-        callback(error)
-      } else {
-        let venues = data.venues
-        let list = []
+  searchRestaurants(params, callback)
+}
 
-        for (let i = 0; i < venues.length; i++) {
-          list.push(venues[i].name)
-        }
+function searchRestaurants(params, callback) {
+  params.categoryId = RESTAURANT_CATEGORY
 
-        callback(null, list)
+  foursquare.Venues.search(LOCATION.lattitude, LOCATION.longitude, null, params, null, function(error, data) {
+    if (error) {
+      console.log('Unable to retrieve venues', error)
+      callback(error)
+    } else {
+      let venues = data.venues
+      let list = []
+
+      for (let i = 0; i < venues.length; i++) {
+        list.push({
+          name: venues[i].name
+        })
       }
-    })
-  }
 
-  return {
-    'query' : query
-  }
+      callback(null, list)
+    }
+  })
 }
